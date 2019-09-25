f082fd517a338cd4eee06a6874d689a052387420
imac1080 <imac1080@gmail.com>
2019-09-25 19:05:24 +0200

    first commit

---
+++ src/PT2/ReadFileJava.java
@@ -0,0 +1,11 @@
+package PT2;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ReadFileJava {
+	public static void main(String[] args) {
+		File fichero = new File("prueba.txt");
+		Scanner s = null;
+	}
+}
